a9ad9f58b6e41d61a6ebe06cf06b6c046a73ba77
Ankit Rawat <dev.ankurawat@gmail.com>
2025-07-05 19:11:23 +0530

    Nab Bar Done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <>
+      <div className="flex flex-col w-full bg-gray-100">
+        <Navbar />
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,118 @@
+import { useState, useEffect } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      const menuButton = document.getElementById("menu-button");
+      const mobileMenu = document.getElementById("mobile-menu");
+      if (
+        isMenuOpen &&
+        !menuButton.contains(e.target) &&
+        !mobileMenu.contains(e.target)
+      ) {
+        setIsMenuOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+  }, [isMenuOpen]);
+
+  return (
+    <>
+      <nav className="w-full bg-green-300 flex justify-between items-center px-4 py-3 shadow fixed top-0 z-50 ">
+        <div className="text-2xl font-bold">SANGEETA</div>
+
+     
+        <div className="hidden md:flex gap-4">
+          <a href="#" className="text-lg font-medium hover:text-blue-600">
+            Home
+          </a>
+          <a href="#about" className="text-lg font-medium hover:text-blue-600">
+            About
+          </a>
+          <a
+            href="#serv-heading"
+            className="text-lg font-medium hover:text-blue-600"
+          >
+            Services
+          </a>
+          <a href="#" className="text-lg font-medium hover:text-blue-600">
+            Achievement
+          </a>
+          <a
+            href="#contact-us"
+            className="text-lg font-medium hover:text-blue-600"
+          >
+            Contact
+          </a>
+          <a href="#" className="text-lg font-medium hover:text-blue-600">
+            Testimonial
+          </a>
+        </div>
+
+        <button
+          id="menu-button"
+          className="md:hidden flex items-center"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          {isMenuOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+        </button>
+      </nav>
+
+     
+      <div
+        id="mobile-menu"
+        className={`absolute top-[50%] left-[50%] w-full bg-green-300 flex flex-col items-center gap-4 py-5 shadow-md transition-transform duration-300 md:hidden ${
+          isMenuOpen
+            ? "translate-x-[-50%] translate-y-[-50%] opacity-100"
+            : "translate-x-[-50%] translate-y-[-100%] opacity-0 hidden"
+        } `}
+      >
+        <a
+          href="#"
+          className="text-lg font-medium"
+          onClick={() => setIsMenuOpen(false)}
+        >
+          Home
+        </a>
+        <a
+          href="#about"
+          className="text-lg font-medium"
+          onClick={() => setIsMenuOpen(false)}
+        >
+          About
+        </a>
+        <a
+          href="#serv-heading"
+          className="text-lg font-medium"
+          onClick={() => setIsMenuOpen(false)}
+        >
+          Services
+        </a>
+        <a
+          href="#"
+          className="text-lg font-medium"
+          onClick={() => setIsMenuOpen(false)}
+        >
+          Achievement
+        </a>
+        <a
+          href="#contact-us"
+          className="text-lg font-medium"
+          onClick={() => setIsMenuOpen(false)}
+        >
+          Contact
+        </a>
+        <a
+          href="#"
+          className="text-lg font-medium"
+          onClick={() => setIsMenuOpen(false)}
+        >
+          Testimonial
+        </a>
+      </div>
+    </>
+  );
+}
